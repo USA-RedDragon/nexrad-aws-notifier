9380e3ccf7bf6ca2deafe427b9b54d894beac0c6
Jacob McSwain <jacob.a.mcswain@gmail.com>
2024-04-19 22:06:06 -0500

    config: add some tests for env processing and tracing

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -16,3 +16,24 @@ func TestExampleConfig(t *testing.T) {
 		t.Errorf("unexpected error: %v", err)
 	}
 }
+
+func TestTracing(t *testing.T) {
+	t.Parallel()
+	baseCmd := cmd.NewCommand("testing", "deadbeef")
+	// Avoid port conflict
+	baseCmd.SetArgs([]string{"--http.port", "8085", "--http.metrics.port", "8086", "--http.tracing.enabled", "true"})
+	err := baseCmd.Execute()
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+}
+
+func TestEnvConfig(t *testing.T) {
+	t.Setenv("HTTP__PORT", "8087")
+	t.Setenv("HTTP__METRICS__PORT", "8088")
+	baseCmd := cmd.NewCommand("testing", "deadbeef")
+	err := baseCmd.Execute()
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+}
